e1d3d82aea44fbc7212a444627c242b9820b3fa3
Jason Asselin <jason.asselin@cdid.com>
2019-12-03 21:41:41 -0500

    Missing file

---
+++ ASPNETCoreRuntimeCompilation/FeatureRuntimeCompilation/Mvc/FeatureEndpointSelector.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing.Matching;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreRuntimeCompilation.FeatureRuntimeCompilation.Mvc
+{
+    public class FeatureEndpointSelector : EndpointSelector
+    {
+        public override Task SelectAsync(HttpContext httpContext, CandidateSet candidates)
+        {
+            var candidate = candidates[candidates.Count - 1]; //TODO: get current assembly
+            httpContext.SetEndpoint(candidate.Endpoint);
+            httpContext.Request.RouteValues = candidate.Values;
+            return Task.CompletedTask;
+        }
+    }
+}
